007ce5915f6c9f357db1481bad29c4e5de4edb3c
sritee <thiagarajan14111@mech.ssn.edu.in>
2020-02-28 13:51:57 -0800

    skip rows  with arm failure

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -8,9 +8,10 @@ Created on Sat Feb 22 18:58:58 2020
 
 import pandas as pd
 import numpy as np
-
+import datetime
 
 file_range = [1, 20]
+filter_arm_failure = True
 
 
 data = []
@@ -25,7 +26,10 @@ data = pd.concat(data, ignore_index = True)
 
 log_durations = ['Base Planning', 'Base Navigation', 'Arm Planning', 'Arm Execution']
 
-data['Arm Execution Failure'] = data['Arm Execution End'] == 0
+
+if filter_arm_failure:
+   data = data.loc[data['Arm Execution End'] != 0]
+
 data.loc[data['Arm Execution End'] == 0, 'Arm Execution End'] = datetime.datetime.now() #just set it to current time, means fail
 
 for task in log_durations:
@@ -34,3 +38,17 @@ for task in log_durations:
     #print(task)
     data[task + ' Duration'] = data[task + ' Duration'].apply(lambda k : k.total_seconds())
 
+
+'''
+Available columns for reference
+Index([u'Base Planning Start', u'Base Planning End', u'Base Planning Goal',
+       u'Base Navigation Start', u'Base Navigation End',
+       u'Base Navigation Start Pose', u'Base Navigation End Pose',
+       u'Arm Planning Start', u'Arm Planning End', u'Arm Planning Success',
+       u'Arm Execution Start Pose', u'Arm Execution End Pose',
+       u'Arm Execution Start', u'Arm Execution End', u'Success',
+       u'Base Planning Duration', u'Base Navigation Duration',
+       u'Arm Planning Duration', u'Arm Execution Duration'],
+      dtype='object')
+
+'''
